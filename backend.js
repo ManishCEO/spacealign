03de2fb516fb6af8bbef8675677debca754c2ab8
ManishCEO <bhardawajmanish358@gmail.com>
2025-07-08 14:13:47 +0530

    Update backend.js
    port 3306

--- backend.js
+++ backend.js
@@ -10,7 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true })); // Important for form fields
-const PORT = 7000;
+//const PORT = 7000;
+const PORT = 3306;
 
 
 
