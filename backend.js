fa00d03a6c9dbf113a493c15fab99d1c5b2e41ec
ManishCEO <bhardawajmanish358@gmail.com>
2025-07-08 14:23:23 +0530

    Update backend.js

--- backend.js
+++ backend.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true })); // Important for form fields
 //const PORT = 7000;
-const PORT = 3306;
+const PORT = 7000;
 
 
 
